b6b754ad53300e8bf56442d0ed9b6cbd9bbe189f
baninations <baninations@gmail.com>
2023-06-22 22:59:00 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+function newItem() {
+  let li = $("<li></li>");
+  let inputValue = $("#input").val();
+  li.append(inputValue);
+
+  if (inputValue === "") {
+    alert("You must write something!");
+  } else {
+    let list = document.querySelector("#list");
+    $("#list").append(li);
+    $("#input").val("");
+  }
+
+  function crossOut() {
+    li.toggleClass("strike");
+  }
+
+  li.on("dblclick", function crossOut() {
+    li.toggleClass("strike");
+  });
+
+  let crossOutButton = $("<crossOutButton></crossOutButton>");
+  crossOutButton.append(document.createTextNode("X"));
+  li.append(crossOutButton);
+
+  crossOutButton.on("click", deleteListItem);
+
+  function deleteListItem() {
+    li.addClass("delete");
+  }
+  $("#list").sortable();
+}
+
+let input = $("#input");
+
+input.on("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    $("#button").click();
+    $("#input").val("");
+  }
+});
